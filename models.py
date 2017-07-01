419f18bbf33992dcad47bb44906b138f65821480
Josh Reed <jreed73@vols.utk.edu>
2017-06-30 22:28:47 -0400

    Added Reginald and transactions

--- models.py
+++ models.py
@@ -3,7 +3,10 @@ import datetime
 from shutil import copyfile
 import PIL.Image
 from flask import current_app
+from flask_login import login_required, current_user
+
 import os
+import statistics
 
 #General models notes
 #==================================================================================================
@@ -51,8 +54,89 @@ class User(db.Model):
 	
 	def get_id(self):
 		return self.id
-
 		
+class Transaction(db.Model):
+	
+	__tablename__ = "transaction"
+	
+	id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True)	# Unique ID
+	amt = db.Column(db.Numeric(precision=11, scale=2), nullable=False)				# The amount in dollars
+	income = db.Column(db.Boolean, nullable=False)									# Whether it's income (1 if income)
+	type = db.Column(db.Integer, nullable=False)									# ID of Type (see Type)
+	time = db.Column(db.DateTime, nullable=False)									# Time of notification of transaction
+	srcdest = db.Column(db.String(127), nullable=True)								# The optional source or destination (i.e. Kroger's)
+	desc = db.Column(db.String(255), nullable=True)									# An optional description
+	user = db.Column(db.Integer, nullable=False)									# The user who made this record
+
+	#Create a new record. Amount, income, and type are required. Does not add to database.
+	def __init__(self, amt, income, type, srcdest = None, desc = None):
+		self.amt = amt
+		self.income = income
+		self.type = type
+		
+		self.time = datetime.datetime.now()
+		self.user = current_user.id
+		
+		self.srcdest = srcdest
+		self.desc = desc
+	
+	#Gets a list (strings) of up to the n most commonly used (source or destination, depending on src t/f) for the current user. 
+	#If none repeat, returns the most recent.
+	@staticmethod
+	def get_common_srcdest(n, src):
+		dests = [trans.srcdest for trans in Transaction.query.filter_by(user = current_user.id).filter_by(income = src).order_by('-time').all()] # All destinations for this user.
+		print dests
+		if(len(dests) < n):
+			n = len(dests)
+		list = []
+		for c in range(n):
+			if(len(dests) == 0):
+				break
+			dest = None
+			try:
+				dest = statistics.mode(dests)
+			except statistics.StatisticsError: # If there is no most common object (or the set is empty, which it should not be).
+				dest = dests[0]
+			
+			dests = [d for d in dests if d != dest] # Filter dest from the list
+			list.append(dest)
+		return list
+	
+	#Get a string representation of this entry.
+	def __repr__(self):
+		return getStr(self)
+		
+	#Gets the row as a dictionary {colName1: colVal1, colName2: colVal2, ... ETC}
+	#May have to add compatibility for DateTime and other expressions later.
+	#Columns, if provided, should be a list of column names to include. Otherwise will return all.
+	def getDict(self, columns=None):
+		return getDict(self, columns)
+	
+class Type(db.Model):
+	
+	__tablename__ = "trans_type"
+	
+	id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True)	# Unique ID
+	name = db.Column(db.String(32), nullable=False)									# Name of this type of transaction
+	desc = db.Column(db.String(127), nullable=False)								# Short description for this trans
+	income = db.Column(db.Boolean, nullable=False, default=False)					# Whether it's income
+	
+	def __init__(self, name, desc, income):
+		self.name = name
+		self.desc = desc
+		self.income = income
+	
+class Method(db.Model):
+	
+	__tablename__ = "trans_method"
+	
+	id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True)	# Unique ID
+	name = db.Column(db.String(32), nullable=True)									# Name of this method (e.g. cash, debit, etc.)
+	
+	
+	def __init__(self, name):
+		self.name = name
+
 class Device(db.Model):
 	
 	__tablename__ = "dev"
