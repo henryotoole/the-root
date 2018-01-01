b7424b7f99106a2291e366c59a66d42669d05a92
Josh Reed <jreed73@vols.utk.edu>
2017-12-31 19:59:00 -0500

    Add untether

--- __init__.py
+++ __init__.py
@@ -16,11 +16,12 @@ def setup():
 	register_extensions(app)
 	#Import all path-containing files
 	import the_root.landing	# The landing and login pages
-	import the_root.bookkeeper # An app for handling transaction records.
+	import the_root.bookkeeper # An app for handling finances.
+	import the_root.untether # An app for taking notes.
 	import the_root.projects # The project viewer app
 	import the_root.archive # The archive portion of the site.
 	import the_root.serverop # A set of functions for running servers via flask as an interface and controller
-	import the_root.launcher # Handles launching apps and outward-facing user account stuff.
+	import the_root.account # Handles all outward-facing accounts stuff
 	import the_root.admin # Admin stuff and admin decorator
 	#Setup debug, if necessary.
 	if(app.config['DEBUG']):
--- landing.py
+++ landing.py
@@ -9,11 +9,6 @@ import flask
 from flask_login import login_required, login_user, logout_user, current_user
 
 from flask import render_template, send_file
-
-@login_manager.user_loader
-def load_user(user_id):
-	#Gets an instance of User based off a specific row of the database.
-	return User.query.filter_by(id=user_id).first()
 	
 @app.route("/")
 def landing():
@@ -23,43 +18,11 @@ def landing():
 def robots():
 	return send_file('robots.txt')
 
-
-@app.route("/logout", methods=['GET', 'POST'])
-@login_required # This must ALWAYS go below the route decorator! Otherwise anyn users can log in
-def logout():
-	logout_user()
-	print 'Logged out'
-	return flask.redirect("/") 
-
-#NOTE!!: If both GET and POST are not allowed, wtforms will trip a 405 error.
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-	sp_local = app.config['STATIC_URL_LOCAL']
-	loginform = LoginForm()
-	if loginform.validate_on_submit():
-		email = loginform.email.data
-		password = loginform.password.data
-		
-		#Encryption and stuff goes here
-		
-		#Get an instance
-		user = User.query.filter_by(email=email).first()
-		if user:
-			if user.validate_password(password):
-				#Check password, eventually
-				login_user(user)
-				
-				
-				return flask.redirect("/reginald")
-			else:
-				return "Bad password"
-		
-		#No user of that name.
-		return "Not a valid account"
-	#As one would expect, if there are form errors this will not be true.
-	else:
-		return render_template('login.html', loginform=loginform, sp_local=sp_local)
-
 @app.route("/about")
 def about():
 	return render_template("/about.html", sp_local=app.config['STATIC_URL_LOCAL'], sp_content=app.config['STATIC_URL_CONTENT'])
+	
+#The main launcher page with the app listing.
+@app.route("/launcher", methods=['GET', 'POST'])
+def launcher():
+	return render_template("/launcher.html", sp_local=app.config['STATIC_URL_LOCAL'], sp_content=app.config['STATIC_URL_CONTENT'])
